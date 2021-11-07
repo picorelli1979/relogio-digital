022719c75138c5118db0a3d22d3be155854eb897
picorelli1979 <93301145+picorelli1979@users.noreply.github.com>
2021-11-06 22:19:04 -0300

    Add files via upload

---
+++ Relogio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+function Relogio(){
+    return (
+      <h2>
+        { new Date().toLocaleTimeString()}
+      </h2>
+    );
+
+  }
+  
+  export default Relogio;
